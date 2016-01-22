bd34cb63d8a1c48910d0bdbe550824e5edde741b
Juan Felipe Arias <juantrullo@gmail.com>
2016-01-21 20:20:01 -0500

    jfa-21-01-2016 - Se arregla el movimiento doble del personaje al correr - Se desactian los generadores de autos y enemigos para crear oleadas

---
+++ Assets/MisScripts/GeneradorVehiculos.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneradorVehiculos : MonoBehaviour {
+
+	public GameObject[] arrayVehiculos;
+	public float velocidadGeneradorCarro = 5f;
+	public float tiempoInicial = 1f;
+	public float tiempoConstante = 2f;
+	public int posicionCarro;
+	// Use this for initialization
+	void Start () {
+		posicionCarro = 0;//Random.Range (0, 0);
+		InvokeRepeating("InvocarCars",tiempoInicial,tiempoConstante);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovimientoGeneradorCarro();
+	}
+
+	void MovimientoGeneradorCarro(){
+		transform.Translate (velocidadGeneradorCarro*Time.deltaTime*1,0,0);
+	}
+
+	void InvocarCars(){
+		Instantiate(arrayVehiculos[posicionCarro],new Vector3(transform.position.x,transform.position.y+2,transform.position.z),Quaternion.identity);
+	}
+}
---
+++ Assets/MisScripts/MoveCars.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCars : MonoBehaviour {
+
+	public float velocityCar = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveCar();
+	}
+
+	void MoveCar(){
+		transform.Translate((velocityCar*Time.deltaTime)*-1,0,0);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag=="Bullet"){
+			Debug.Log ("Entro a la colisión");
+		}
+	}
+
+	void Death(){
+
+	}
+}
